b05bf93b52f835a8aa98a3a736eca4a782a87ff5
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-23 19:42:03 +0700

    menambahkan blok fungsi dalam getArticlesbyId untuk memuat data dari cache terlebih dahulu sebelum melakukan request ke server api

--- js/api.js
+++ js/api.js
@@ -77,6 +77,28 @@ function getArticles() {
 }
 
 function getArticleById() {
+    if ("caches" in window) {
+        caches.match(base_url + "article/" + idParam).then(function(response) {
+            if (response) {
+                response.json().then(function(data) {
+                    var articleHTML = `
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img src="${data.result.cover}" />
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title">${data.result.post_title}</span>
+                        ${snarkdown(data.result.post_content)}
+                    </div>
+                </div>
+                `;
+                    // Sisipkan komponen card ke dalam elemen dengan id #content
+                    document.getElementById("body-content").innerHTML = articleHTML;
+                });
+            }
+        });
+    }
+
     // Ambil nilai query parameter (?id=)
     var urlParams = new URLSearchParams(window.location.search);
     var idParam = urlParams.get("id");
