fb1ca72acd83420fbabd12692fa0a1395f2e8e08
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-23 19:23:51 +0700

    menambahkan fungsi untuk mengunduh detail artikel

--- js/api.js
+++ js/api.js
@@ -21,10 +21,6 @@ function error(error) {
 }
 // Blok kode untuk melakukan request data json
 function getArticles() {
-    // Ambil nilai query parameter (?id=)
-    var urlParams = new URLSearchParams(window.location.search);
-    var idParam = urlParams.get("id");
-
     fetch(base_url + "articles")
         .then(status)
         .then(json)
@@ -51,4 +47,31 @@ function getArticles() {
             document.getElementById("articles").innerHTML = articlesHTML;
         })
         .catch(error);
+}
+
+function getArticleById() {
+    // Ambil nilai query parameter (?id=)
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
+    fetch(base_url + "article/" + idParam)
+        .then(status)
+        .then(json)
+        .then(function(data) {
+            // Objek JavaScript dari response.json() masuk lewat variabel data.
+            console.log(data);
+            // Menyusun komponen card artikel secara dinamis
+            var articleHTML = `
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${data.result.cover}" />
+                </div>
+                <div class="card-content">
+                    <span class="card-title">${data.result.post_title}</span>
+                    ${snarkdown(data.result.post_content)}
+                </div>
+            </div>
+            `;
+            // Sisipkan komponen card ke dalam elemen dengan id #content
+            document.getElementById("body-content").innerHTML = articleHTML;
+        });
 }
