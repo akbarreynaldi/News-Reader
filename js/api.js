54f246636a8a72e38d8b7abde560ea13c2100848
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-23 19:35:41 +0700

    menambahkan blok fungsi dalam getArticles untuk memuat data dari cache terlebih dahulu sebelum melakukan request ke server api

--- js/api.js
+++ js/api.js
@@ -21,6 +21,33 @@ function error(error) {
 }
 // Blok kode untuk melakukan request data json
 function getArticles() {
+    if ('caches' in window) {
+        caches.match(base_url + "articles").then(function(response) {
+            if (response) {
+                response.json().then(function(data) {
+                    var articlesHTML = "";
+                    data.result.forEach(function(article) {
+                        articlesHTML += `
+                        <div class="card">
+                            <a href="./article.html?id=${article.id}">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                <img src="${article.thumbnail}" />
+                                </div>
+                            </a>
+                            <div class="card-content">
+                                <span class="card-title truncate">${article.title}</span>
+                                <p>${article.description}</p>
+                            </div>
+                        </div>
+                    `;
+                    });
+                    // Sisipkan komponen card ke dalam elemen dengan id #content
+                    document.getElementById("articles").innerHTML = articlesHTML;
+                })
+            }
+        })
+    }
+
     fetch(base_url + "articles")
         .then(status)
         .then(json)
