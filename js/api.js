58911fb6431f03c8b4d4a4ad043f0de544722600
akbarreynaldi <akbarreynaldi77@gmail.com>
2020-06-24 19:11:33 +0700

    Refactor Fungsi getArticleById Untuk Mengembalikan Sebuah Promise

--- js/api.js
+++ js/api.js
@@ -82,15 +82,17 @@ function getArticles() {
 }
 
 function getArticleById() {
-    // Ambil nilai query parameter (?id=)
-    var urlParams = new URLSearchParams(window.location.search);
-    var idParam = urlParams.get("id");
+    return new Promise(function(resolve, reject) {
 
-    if ("caches" in window) {
-        caches.match(base_url + "article/" + idParam).then(function(response) {
-            if (response) {
-                response.json().then(function(data) {
-                    var articleHTML = `
+        // Ambil nilai query parameter (?id=)
+        var urlParams = new URLSearchParams(window.location.search);
+        var idParam = urlParams.get("id");
+
+        if ("caches" in window) {
+            caches.match(base_url + "article/" + idParam).then(function(response) {
+                if (response) {
+                    response.json().then(function(data) {
+                        var articleHTML = `
                         <div class="card">
                             <div class="card-image waves-effect waves-block waves-light">
                                 <img src="${data.result.cover}" />
@@ -101,21 +103,22 @@ function getArticleById() {
                             </div>
                         </div>
                         `;
-                    // Sisipkan komponen card ke dalam elemen dengan id #content
-                    document.getElementById("body-content").innerHTML = articleHTML;
-                });
-            }
-        });
-    }
+                        // Sisipkan komponen card ke dalam elemen dengan id #content
+                        document.getElementById("body-content").innerHTML = articleHTML;
+                        resolve(data);
+                    });
+                }
+            });
+        }
 
-    fetch(base_url + "article/" + idParam)
-        .then(status)
-        .then(json)
-        .then(function(data) {
-            // Objek JavaScript dari response.json() masuk lewat variabel data.
-            console.log(data);
-            // Menyusun komponen card artikel secara dinamis
-            var articleHTML = `
+        fetch(base_url + "article/" + idParam)
+            .then(status)
+            .then(json)
+            .then(function(data) {
+                // Objek JavaScript dari response.json() masuk lewat variabel data.
+                console.log(data);
+                // Menyusun komponen card artikel secara dinamis
+                var articleHTML = `
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="${data.result.cover}" />
@@ -126,7 +129,9 @@ function getArticleById() {
                     </div>
                 </div>
                 `;
-            // Sisipkan komponen card ke dalam elemen dengan id #content
-            document.getElementById("body-content").innerHTML = articleHTML;
-        });
+                // Sisipkan komponen card ke dalam elemen dengan id #content
+                document.getElementById("body-content").innerHTML = articleHTML;
+                resolve(data);
+            });
+    });
 }
